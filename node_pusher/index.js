a87c2dbf215484cfd88511b39575853b46136211
Amri Karisma <amrikarisma@live.com>
2023-04-04 17:20:07 +0700

    update web rtc

--- node_pusher/index.js
+++ node_pusher/index.js
@@ -14,7 +14,8 @@ const pusher = new Pusher({
     appId: process.env.APP_ID,
     key: process.env.APP_KEY,
     secret: process.env.APP_SECRET,
-    cluster: process.env.APP_CLUSTER
+    cluster: process.env.APP_CLUSTER,
+    encrypted: true
 });
 
 app.get('/', (req, res) => {
