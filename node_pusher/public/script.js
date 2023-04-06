4509fe84284018d4a65a1922bc8cdb7343b45551
Amri Karisma <amrikarisma@live.com>
2023-04-06 16:57:22 +0700

    update iceServers

--- node_pusher/public/script.js
+++ node_pusher/public/script.js
@@ -154,29 +154,8 @@ async function prepareCaller() {
     const serverConfig = {
         iceServers: [
             {
-                urls: [
-                    'stun:stun.l.google.com:19302',
-                    'stun:stun1.l.google.com:19302',
-                    'stun:stun2.l.google.com:19302',
-                    'stun:stun3.l.google.com:19302',
-                    'stun:stun4.l.google.com:19302',
-                ],
-            },
-            {
-                urls: 'turn:numb.viagenie.ca',
-                credential: 'muazkh',
-                username: 'webrtc@live.com',
-            },
-            {
-                urls: 'turn:numb.viagenie.ca',
-                credential: 'muazkh',
-                username: 'webrtc@live.com',
-            },
-            {
-                urls: 'turn:192.158.29.39:3478?transport=udp',
-                credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-                username: '28224511:1379330808',
-            },
+                urls: 'stun:stun.l.google.com:19302',
+            }
         ],
     }
 
